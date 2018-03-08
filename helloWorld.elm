500031a26340a5a9c494642f5b6e50f29fa0454b
Shingo Konnai <shingokonnai@Shingos-MacBook.local>
2018-03-08 13:55:57 +0900

    still writing add part

--- helloWorld.elm
+++ helloWorld.elm
@@ -1,63 +1,7 @@
-import Html exposing (Html, div, li, ul, text, input, program, button)
+import Html exposing (Html, div, li, ul, text, input, table, thead, program, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Html.Events exposing (onClick)
--- MODEL
-type alias Name = 
-  { firstname : String
-  , lastname : String
-  }
-
-type alias Model = List Name
-
-init : ( Model, Cmd Msg )
-init = 
-  ( [ { firstname = "Meowze", lastname = "Dong" }
-    , { firstname = "Hana", lastname = "March" }
-    , { firstname = "Mia", lastname = "Kirmse"} ]
-    , Cmd.none)
-
--- MESSAGES
-
-type Msg
-  = Add
-  -- | Firstname String
-  -- | Lastname String
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div [] [ ul [] (List.map viewGreeting model)
-  -- , input [ type_ "text", placeholder "firstname", onInput Firstname ] []
-  -- , input [ type_ "text", placeholder "lastname", onInput Lastname ] []
-  , button [onClick Add] [ text "Add" ]
-  ] 
-
-viewGreeting : { a | firstname : String, lastname : String } -> Html Msg
-viewGreeting greeting =
-  div [] 
-    [ li [] [ text greeting.firstname, text " ", text greeting.lastname ] ]
-
--- UPDATE
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-  case msg of
-    Add ->
-      let 
-        updatedModel =
-          { firstname = "henoheno", lastname = "moheji" } :: model
-      in
-      ( updatedModel, Cmd.none )
-    -- Firstname firstname ->
-    --   ( { model | firstname = firstname }, Cmd.none )
-    -- Lastname lastname ->
-    --   ( { model | lastname = lastname }, Cmd.none ) 
--- SUBSCRIPTIONS
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
 
 -- MAIN
 main : Program Never Model Msg
@@ -68,3 +12,90 @@ main =
     , update = update
     , subscriptions = subscriptions
     }
+
+-- MODEL
+type alias Model =
+  { persons : List Person
+  , id : ID
+  , firstname : Name
+  , lastname : Name
+  }
+
+init : ( Model, Cmd Msg )
+init = 
+  ( { persons = [
+      { id = 1, firstname = "Meowze", lastname = "Dong" }
+      , { id = 2, firstname = "Hana", lastname = "March" }
+      , { id = 3, firstname = "Mia", lastname = "Kirmse"}
+      ]
+    , firstname = ""
+    , lastname = ""
+    }
+  , Cmd.none  
+  )
+
+type alias Person = 
+  { id : ID 
+  , firstname : String 
+  , lastname : String
+  }
+
+type alias ID =
+  Int
+
+type alias Name = 
+  String
+
+-- MESSAGES
+
+type Msg
+  = Id ID
+  | FirstName Name
+  | LastName Name
+  | NewPerson
+
+-- UPDATE
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg ({ persons, firstname, lastname } as model) =
+  case msg of
+    Id id -> 
+      ( { model | id = id }, Cmd.none )
+    
+    FirstName name ->
+      ( { model | firstname = firstname }, Cmd.none )
+      
+    LastName name ->
+      ( { model | lastname = lastname }, Cmd.none )
+      
+    NewPerson ->
+      ( { model | persons = { id = id, fitstname = firstname, lastname = lastname } :: persons }, Cmd.none )
+
+-- VIEW
+
+view : Model -> Html Msg
+view ({ persons, firstname, lastname } as model) =
+  div [] [ addForm id name ] 
+
+addForm : Name -> ID -> Html Msg
+addForm name id =
+  div []
+    [ div [] [ input [ type_ "text", placeholder "firstname" onInput FirstName ] [] ]
+    , div [] [ input [ type_ "text", placeholder "lsstname" onInput LastName ] [] ]
+    , div [] [ button [onClick NewPerson] [ text "Add" ] ]
+    ]
+
+-- personList : List Person -> ID -> Name -> Html Msg
+-- personList persons id name =
+  
+
+--  person : 
+--     [ li [] [ text firstname, text " ", text lastname ] ]
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
